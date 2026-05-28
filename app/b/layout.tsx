506e04a36f0d2c83524bf254d2b2fb3adb37c4f2
Itxsahil <sahilkh9087@gmail.com>
2026-05-29 00:41:42 +0530

    x card

--- app/b/layout.tsx
+++ app/b/layout.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  twitter: {
+    card: "summary_large_image",
+  },
+}
+
 export default function RootLayout({
   children
 }: Readonly<{
