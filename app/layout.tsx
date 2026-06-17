a5c937fc2be8aca4f8047e8d36477b7059f64809
Sahil <132879371+Itxsahil@users.noreply.github.com>
2026-06-17 10:42:44 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,14 @@ export const metadata: Metadata = {
     title: "Sahil Khan",
     description:
       "Full-Stack Developer & Technical Writer. Showcasing web development projects, insightful blog posts, and creative problem solving with modern tech stacks.",
+    images: [
+      {
+        url: "https://sahilkhan.site/og/home-og.png",
+        width: 800,
+        height: 600,
+        alt: "Sahil Khan",
+      },
+    ],
   },
   robots: {
     index: true,
